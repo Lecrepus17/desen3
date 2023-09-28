ac8f16e0eb68ff2ddc7120dc7a2471af515459c8
Gustavo Rajab <122064946+gustajab@users.noreply.github.com>
2023-09-28 13:35:53 -0300

    t

--- document/app/Http/Controllers/UserController.php
+++ document/app/Http/Controllers/UserController.php
@@ -3,8 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+    public function criarUsuario(Request $request)
+    {
+        // Valide os dados recebidos do formulário
+        $request->validate([
+            'nome' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'senha' => 'required|string|min:8',
+        ]);
+
+        // Crie um novo usuário
+        $user = new User();
+        $user->nome = $request->input('nome');
+        $user->email = $request->input('email');
+        $user->senha = bcrypt($request->input('senha'));
+
+        $user->save();
+
+        // Redirecione ou retorne uma resposta de sucesso
+       // return redirect('/usuarios')->with('success', 'Usuário criado com sucesso!');
+    }
+
 }
